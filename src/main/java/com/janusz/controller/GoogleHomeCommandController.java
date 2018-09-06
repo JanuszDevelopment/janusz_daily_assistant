a7ec2454ca7a40966b6f9f8d2c2da039ce47d5d9
Łukasz <klarkq@gmail.com>
2018-09-06 19:39:12 +0200

    Add sample response

--- src/main/java/com/janusz/controller/GoogleHomeCommandController.java
+++ src/main/java/com/janusz/controller/GoogleHomeCommandController.java
@@ -13,6 +13,67 @@ public class GoogleHomeCommandController {
 
     @RequestMapping(value = "/parseCommand", method = POST)
     public String index() {
-        return "Greetings from Spring Boot!";
+        System.out.println("Shoot");
+        return testResposne();
     }
+
+    private String testResposne() {
+        return "{\n" +
+                "  \"fulfillmentText\": \"This is a text response\",\n" +
+                "  \"fulfillmentMessages\": [\n" +
+                "    {\n" +
+                "      \"card\": {\n" +
+                "        \"title\": \"card title\",\n" +
+                "        \"subtitle\": \"card text\",\n" +
+                "        \"imageUri\": \"https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png\",\n" +
+                "        \"buttons\": [\n" +
+                "          {\n" +
+                "            \"text\": \"button text\",\n" +
+                "            \"postback\": \"https://assistant.google.com/\"\n" +
+                "          }\n" +
+                "        ]\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"source\": \"example.com\",\n" +
+                "  \"payload\": {\n" +
+                "    \"google\": {\n" +
+                "      \"expectUserResponse\": true,\n" +
+                "      \"richResponse\": {\n" +
+                "        \"items\": [\n" +
+                "          {\n" +
+                "            \"simpleResponse\": {\n" +
+                "              \"textToSpeech\": \"this is a simple response\"\n" +
+                "            }\n" +
+                "          }\n" +
+                "        ]\n" +
+                "      }\n" +
+                "    },\n" +
+                "    \"facebook\": {\n" +
+                "      \"text\": \"Hello, Facebook!\"\n" +
+                "    },\n" +
+                "    \"slack\": {\n" +
+                "      \"text\": \"This is a text response for Slack.\"\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"outputContexts\": [\n" +
+                "    {\n" +
+                "      \"name\": \"projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name\",\n" +
+                "      \"lifespanCount\": 5,\n" +
+                "      \"parameters\": {\n" +
+                "        \"param\": \"param value\"\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"followupEventInput\": {\n" +
+                "    \"name\": \"event name\",\n" +
+                "    \"languageCode\": \"en-US\",\n" +
+                "    \"parameters\": {\n" +
+                "      \"param\": \"param value\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+    }
+
+
 }
