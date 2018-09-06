5a6ae67e102f1a22b4999b0b379efe5309188fd3
Łukasz <klarkq@gmail.com>
2018-09-06 19:59:37 +0200

    Add simpler response

--- src/main/java/com/janusz/controller/GoogleHomeCommandController.java
+++ src/main/java/com/janusz/controller/GoogleHomeCommandController.java
@@ -19,23 +19,6 @@ public class GoogleHomeCommandController {
 
     private String testResposne() {
         return "{\n" +
-                "  \"fulfillmentText\": \"This is a text response\",\n" +
-                "  \"fulfillmentMessages\": [\n" +
-                "    {\n" +
-                "      \"card\": {\n" +
-                "        \"title\": \"card title\",\n" +
-                "        \"subtitle\": \"card text\",\n" +
-                "        \"imageUri\": \"https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png\",\n" +
-                "        \"buttons\": [\n" +
-                "          {\n" +
-                "            \"text\": \"button text\",\n" +
-                "            \"postback\": \"https://assistant.google.com/\"\n" +
-                "          }\n" +
-                "        ]\n" +
-                "      }\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"source\": \"example.com\",\n" +
                 "  \"payload\": {\n" +
                 "    \"google\": {\n" +
                 "      \"expectUserResponse\": true,\n" +
@@ -48,28 +31,6 @@ public class GoogleHomeCommandController {
                 "          }\n" +
                 "        ]\n" +
                 "      }\n" +
-                "    },\n" +
-                "    \"facebook\": {\n" +
-                "      \"text\": \"Hello, Facebook!\"\n" +
-                "    },\n" +
-                "    \"slack\": {\n" +
-                "      \"text\": \"This is a text response for Slack.\"\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"outputContexts\": [\n" +
-                "    {\n" +
-                "      \"name\": \"projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name\",\n" +
-                "      \"lifespanCount\": 5,\n" +
-                "      \"parameters\": {\n" +
-                "        \"param\": \"param value\"\n" +
-                "      }\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"followupEventInput\": {\n" +
-                "    \"name\": \"event name\",\n" +
-                "    \"languageCode\": \"en-US\",\n" +
-                "    \"parameters\": {\n" +
-                "      \"param\": \"param value\"\n" +
                 "    }\n" +
                 "  }\n" +
                 "}";
