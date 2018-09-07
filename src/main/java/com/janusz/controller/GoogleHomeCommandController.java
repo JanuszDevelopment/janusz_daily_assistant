0105e343ab5f31d0716dd37b426c86df36969f4c
Dmytro Melnychuk <melnichukdv@gmail.com>
2018-09-07 09:20:19 +0200

    Use conversation token

--- src/main/java/com/janusz/controller/GoogleHomeCommandController.java
+++ src/main/java/com/janusz/controller/GoogleHomeCommandController.java
@@ -19,57 +19,12 @@ public class GoogleHomeCommandController {
 
     private String testResposne() {
         return "{\n" +
-                "  \"fulfillmentText\": \"This is a text response\",\n" +
-                "  \"fulfillmentMessages\": [\n" +
-                "    {\n" +
-                "      \"card\": {\n" +
-                "        \"title\": \"card title\",\n" +
-                "        \"subtitle\": \"card text\",\n" +
-                "        \"imageUri\": \"https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png\",\n" +
-                "        \"buttons\": [\n" +
-                "          {\n" +
-                "            \"text\": \"button text\",\n" +
-                "            \"postback\": \"https://assistant.google.com/\"\n" +
-                "          }\n" +
-                "        ]\n" +
-                "      }\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"source\": \"example.com\",\n" +
-                "  \"payload\": {\n" +
-                "    \"google\": {\n" +
-                "      \"expectUserResponse\": true,\n" +
-                "      \"richResponse\": {\n" +
-                "        \"items\": [\n" +
-                "          {\n" +
-                "            \"simpleResponse\": {\n" +
-                "              \"textToSpeech\": \"this is a simple response\"\n" +
-                "            }\n" +
-                "          }\n" +
-                "        ]\n" +
-                "      }\n" +
-                "    },\n" +
-                "    \"facebook\": {\n" +
-                "      \"text\": \"Hello, Facebook!\"\n" +
-                "    },\n" +
-                "    \"slack\": {\n" +
-                "      \"text\": \"This is a text response for Slack.\"\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"outputContexts\": [\n" +
-                "    {\n" +
-                "      \"name\": \"projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name\",\n" +
-                "      \"lifespanCount\": 5,\n" +
-                "      \"parameters\": {\n" +
-                "        \"param\": \"param value\"\n" +
-                "      }\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"followupEventInput\": {\n" +
-                "    \"name\": \"event name\",\n" +
-                "    \"languageCode\": \"en-US\",\n" +
-                "    \"parameters\": {\n" +
-                "      \"param\": \"param value\"\n" +
+                "  \"conversation_token\": \"42\",\n" +
+                "  \"expect_user_response\": false,\n" +
+                "\n" +
+                "  \"final_response\": {\n" +
+                "    \"speech_response\": {\n" +
+                "      \"text_to_speech\": \"Thanks for playing!\"\n" +
                 "    }\n" +
                 "  }\n" +
                 "}";
