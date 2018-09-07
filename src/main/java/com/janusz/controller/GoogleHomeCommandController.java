ec2aeb536fe4e7dc95d33531328388a15466bd08
Dmytro Melnychuk <melnichukdv@gmail.com>
2018-09-07 10:37:12 +0200

    Add printing for host

--- src/main/java/com/janusz/controller/GoogleHomeCommandController.java
+++ src/main/java/com/janusz/controller/GoogleHomeCommandController.java
@@ -1,8 +1,5 @@
 package com.janusz.controller;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,24 +11,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RestController
 public class GoogleHomeCommandController {
 
-    @RequestMapping(value = "/parseCommand", method = POST, produces = "application/json")
-    public ResponseEntity<String> index() {
-        System.out.println("Shoot");
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Google-Assistant-API-Version", "v1");
-        return new ResponseEntity<>(testResposne(), headers, HttpStatus.OK);
-    }
-
-    private String testResposne() {
-        return "{\n" +
-                "  \"conversation_token\": \"42\",\n" +
-                "  \"expect_user_response\": false,\n" +
-                "\n" +
-                "  \"final_response\": {\n" +
-                "    \"speech_response\": {\n" +
-                "      \"text_to_speech\": \"Thanks for playing!\"\n" +
-                "    }\n" +
-                "  }\n" +
-                "}";
+    @RequestMapping(value = "/parseCommand", method = POST)
+    public String index() {
+        System.out.println("Received response from Janush daily assistant");
+        return "Success";
     }
 }
